a40650534a50252b380a2588a864554298c6cdb7
pmac1991 <piomac11@gmail.com>
2015-04-13 15:47:02 +0200

    Added Facebook SDK package, in App.xml.cs methods "OnActivated", "OnFacebookAuthenticationFinished", added connection in Package.appxmanifest (facebook id required !!!!) and fb login button in MainPage

--- aplikacjax/aplikacjax.Windows/MainPage.xaml.cs
+++ aplikacjax/aplikacjax.Windows/MainPage.xaml.cs
@@ -4,5 +4,10 @@ namespace aplikacjax
 {
     public sealed partial class MainPage : Page
     {
+        private void OnSessionStateChanged(object sender, Facebook.Client.Controls.SessionStateChangedEventArgs e)
+        {
+            this.ContentPanel.Visibility = (e.SessionState == Facebook.Client.Controls.FacebookSessionState.Opened) ? Windows.UI.Xaml.Visibility.Visible : Windows.UI.Xaml.Visibility.Collapsed;
+                                        
+        }
     }
 }
