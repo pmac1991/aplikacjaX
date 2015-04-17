2b703e7c5452f4574a6754cb8762d72ea8c49af7
pmac1991 <piomac11@gmail.com>
2015-04-18 00:18:27 +0200

    Revert "Additional testing facebook sdk, facebook appid introduction"
    This reverts commit 19ae9357c3429434383480e932f6ed60cf31499c.

--- aplikacjax/aplikacjax.Windows/MainPage.xaml.cs
+++ aplikacjax/aplikacjax.Windows/MainPage.xaml.cs
@@ -1,12 +1,13 @@
 ﻿using Windows.UI.Xaml.Controls;
-using Facebook.Client.Controls;
+
 namespace aplikacjax
 {
     public sealed partial class MainPage : Page
     {
         private void OnSessionStateChanged(object sender, Facebook.Client.Controls.SessionStateChangedEventArgs e)
         {
-            this.ContentPanel.Visibility = (e.SessionState == Facebook.Client.Controls.FacebookSessionState.Opened) ? Windows.UI.Xaml.Visibility.Visible : Windows.UI.Xaml.Visibility.Collapsed;           
+            this.ContentPanel.Visibility = (e.SessionState == Facebook.Client.Controls.FacebookSessionState.Opened) ? Windows.UI.Xaml.Visibility.Visible : Windows.UI.Xaml.Visibility.Collapsed;
+                                        
         }
     }
 }
