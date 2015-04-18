ec40db8bcdf9c66dff2637fed3fd555d4aefdeee
pmac1991 <piomac11@gmail.com>
2015-04-18 11:58:17 +0200

    Reversed facebook sdk login style, switched to solution from MSDN, windows store id is required for this to work

--- aplikacjax/aplikacjax.Windows/MainPage.xaml.cs
+++ aplikacjax/aplikacjax.Windows/MainPage.xaml.cs
@@ -1,8 +1,40 @@
-﻿using Windows.UI.Xaml.Controls;
+﻿using Facebook;
+using System;
+using System.Threading.Tasks;
+using Windows.Security.Authentication.Web;
+using Windows.UI.Xaml.Controls;
 
 namespace aplikacjax
 {
     public sealed partial class MainPage : Page
     {
+        private async Task fbLogin()
+        {
+            //Facebook app id
+            var clientId = "452835904891461";
+            //Facebook permissions
+            var scope = "email";
+
+            var redirectUri = WebAuthenticationBroker.GetCurrentApplicationCallbackUri().ToString();
+            var fb = new FacebookClient();
+            var loginUrl = fb.GetLoginUrl(new
+            {
+                client_id = clientId,
+                redirect_uri = redirectUri,
+                response_type = "token",
+                scope = scope
+            });
+
+            Uri startUri = loginUrl;
+            Uri endUri = new Uri(redirectUri, UriKind.Absolute);
+
+            WebAuthenticationResult result = await WebAuthenticationBroker.AuthenticateAsync(WebAuthenticationOptions.None, startUri, endUri);
+            //await ParseAuthenticationResult(result);
+        }
+
+        private void fb_Login_Click(object sender, Windows.UI.Xaml.RoutedEventArgs e)
+        {
+            fbLogin();
+        }
     }
 }
